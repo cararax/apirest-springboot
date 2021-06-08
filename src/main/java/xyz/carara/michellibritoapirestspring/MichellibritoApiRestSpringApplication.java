e4b8c298da9c8610280cc132f1d4dbdcef958a72
Pedro Carara <cararax@gmail.com>
2021-06-08 12:00:43 -0300

    Criação do projeto spring boot

---
+++ src/main/java/xyz/carara/michellibritoapirestspring/MichellibritoApiRestSpringApplication.java
@@ -0,0 +1,13 @@
+package xyz.carara.michellibritoapirestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichellibritoApiRestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MichellibritoApiRestSpringApplication.class, args);
+	}
+
+}
